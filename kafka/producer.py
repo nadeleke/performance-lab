8fdbf447ed4f61db5d491fdffaf2c5d508ebc3c4
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-01-28 23:57:39 -0800

    read result file lines into kafka

--- kafka/producer.py
+++ kafka/producer.py
@@ -6,6 +6,7 @@
 import argparse
 from kafka import SimpleProducer, KafkaClient
 from time import sleep
+import csv, tarfile, os
 
 def chunk_iterable(A,n):
     '''An iterable that contains the iterates of A divided into lists of size n.
@@ -25,11 +26,13 @@ def chunk_iterable(A,n):
     if len(chunk)>0:
         yield(chunk)
 
+RESULTS_DIR='/var/datamill/results/'
+
 if __name__=="__main__":
 
     # arg parsing
     parser = argparse.ArgumentParser(description="Feed Kafka a stream from a file")
-    parser.add_argument("file", help="A file of data, one datum per line")
+    # parser.add_argument("file", help="A file of data, one datum per line")
     parser.add_argument("host", help="Public IP address of a Kafka node")
     parser.add_argument("topic", help="Kafka topic to feed")
     parser.add_argument("-p", "--port", default=9092, help="port for zookeeper, default 9092")
@@ -42,9 +45,23 @@ if __name__=="__main__":
     kafka = KafkaClient("{0}:{1}".format(args.host, args.port))
     producer = SimpleProducer(kafka)
 
-    # read through the file and send messages to Kafka in chunks
-    with open(args.file, "rb") as f:
-        for chunk in chunk_iterable(f, args.chunksize):
-            print("Sending {0} messages to topic {1} on {2}".format(len(chunk), args.topic, args.host))
-            producer.send_messages(args.topic, *chunk)
-            sleep(1.0*int(args.delay)/1000.0)
+    os.chdir(RESULTS_DIR)
+    for tar_file in os.listdir(RESULTS_DIR):
+        if tar_file.endswith('.tar'):
+            print tar_file
+            tar = tarfile.open(tar_file)
+            tar.extractall()
+            tar.close()
+            file_name = os.path.basename(tar_file)
+            file_name_parts = file_name.split('_')
+            folder_name = file_name_parts[0] + '_' + file_name_parts[1]
+            experiment_id = file_name_parts[1]
+            first_line = True
+            with open('{}experiment_{}/{}_results_index.csv'.format(RESULTS_DIR, experiment_id, experiment_id)) as result_csv:
+                reader = csv.reader(result_csv, delimiter=',')
+                for row in reader:
+                    if first_line:
+                        first_line = False
+                        continue
+                    producer.send_messages(args.topic, row)
+                    sleep(1.0*int(args.delay)/1000.0)
