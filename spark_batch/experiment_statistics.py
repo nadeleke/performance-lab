8d17c44524916e624adc0f3910526be466839b0d
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-01-28 19:16:47 -0800

    calculate averages for times only if the field has distinct values

--- spark_batch/experiment_statistics.py
+++ spark_batch/experiment_statistics.py
@@ -1,8 +1,10 @@
 import csv, tarfile, os
 from pyspark.sql import SQLContext
+from pyspark.sql.functions import *
 sqlContext = SQLContext(sc)
 
 RESULTS_DIR='/home/yuguang/Downloads/' #TODO change to HDFS folder
+RESULTS_DIR='/var/datamill/results/'
 
 os.chdir(RESULTS_DIR)
 for tar_file in os.listdir(RESULTS_DIR):
@@ -15,12 +17,21 @@ for tar_file in os.listdir(RESULTS_DIR):
         folder_name = file_name_parts[0] + '_' + file_name_parts[1]
         experiment_id = file_name_parts[1]
         header = None
-        with open('experiment_{}/{}_results_index.csv'.format(experiment_id, experiment_id)) as result_csv:
+        with open('{}experiment_{}/{}_results_index.csv'.format(RESULTS_DIR, experiment_id, experiment_id)) as result_csv:
             reader = csv.reader(result_csv, delimiter=',')
             for row in reader:
                 if not header:
                     header = row
                     break
-        df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/var/datamill/results/experiment_{}/{}_results_index.csv'.format(experiment_id, experiment_id))
-        for field in header:
-            df.groupBy(field).agg({"run_time":"avg"}).collect()
+        df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('{}experiment_{}/{}_results_index.csv'.format(RESULTS_DIR, experiment_id, experiment_id))
+        for field in [i for i in header if not i.endswith('time')]:
+            if df.select(field).distinct().count() > 1:
+                df.groupBy(field).agg({"setup_time":"avg"})
+                df.groupBy(field).agg({"run_time":"avg"})
+                df.groupBy(field).agg({"collect_time":"avg"})
+
+
+# df.groupBy('sw_swap', 'experiment_id').count('sw_swap').show()
+# df.groupBy('sw_swap', 'experiment_id').agg({"setup_time":"avg"}).rdd
+# df.select('hw_cpu_mhz').agg(countDistinct('hw_cpu_mhz')).show()
+df.select('hw_cpu_mhz').distinct().count()
