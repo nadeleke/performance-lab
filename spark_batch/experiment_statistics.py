d57bd0f3a4499381e8f327b9093c64ab953821c5
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-01-28 16:15:57 -0800

    read result csv file

--- spark_batch/experiment_statistics.py
+++ spark_batch/experiment_statistics.py
@@ -1,5 +1,9 @@
+import csv, tarfile, os
+from pyspark.sql import SQLContext
+sqlContext = SQLContext(sc)
 
-RESULTS_DIR='/home/yuguang/Downloads/'
+RESULTS_DIR='/home/yuguang/Dow #TODO change to HDFS foldernloads/'
+
 os.chdir(RESULTS_DIR)
 for tar_file in os.listdir(RESULTS_DIR):
     if tar_file.endswith('.tar'):
@@ -8,11 +12,14 @@ for tar_file in os.listdir(RESULTS_DIR):
         tar.close()
         file_name = os.path.basename(tar_file)
         file_name_parts = file_name.split('_')
-        folder_name = file_name_parts[0] + '_' + file_name_parts[1]
-        print folder_name
-        file_name_parts = 'experiment_1633'.split('_')
+        folder_name = file_name_parts[0] + '_' + file_name_
+        file_name_parts = 'experiment_1633'.split('_')lit('_')
         experiment_id = file_name_parts[1]
-        with open('experiment_{}/{}_results_index.csv'.format(experiment_id, experiment_id)) as result_csv:
+        with open('experiment_{}/{}_results_index.csv'.format(experiment_id, experiment_id)) as res
             reader = csv.reader(result_csv, delimiter=',')
+            header = None
             for row in reader:
-                print row
+                if not header:
+                    header = row
+                    break
+            print header
