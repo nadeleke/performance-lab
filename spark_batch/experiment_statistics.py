00f09df429254e172a4371f8df7013dce3e98ad0
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-01-28 21:48:02 -0800

    save dataframes to database

--- spark_batch/experiment_statistics.py
+++ spark_batch/experiment_statistics.py
@@ -1,10 +1,14 @@
 import csv, tarfile, os
+import pyspark_cassandra
+from pyspark_cassandra import CassandraSparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 sqlContext = SQLContext(sc)
 
 RESULTS_DIR='/home/yuguang/Downloads/' #TODO change to HDFS folder
 RESULTS_DIR='/var/datamill/results/'
+DATABASE = 'datamill'
+
 
 os.chdir(RESULTS_DIR)
 for tar_file in os.listdir(RESULTS_DIR):
@@ -26,12 +30,13 @@ for tar_file in os.listdir(RESULTS_DIR):
         df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('{}experiment_{}/{}_results_index.csv'.format(RESULTS_DIR, experiment_id, experiment_id))
         for field in [i for i in header if not i.endswith('time')]:
             if df.select(field).distinct().count() > 1:
-                df.groupBy(field).agg({"setup_time":"avg"})
-                df.groupBy(field).agg({"run_time":"avg"})
-                df.groupBy(field).agg({"collect_time":"avg"})
+                for time_field in ['setup', 'run', 'collect']:
+                    performance_metric_field = time_field + '_time'
+                    table_name = 'avg_by_' + field
+                    df.groupBy(field, 'experiment_id').agg({performance_metric_field: "avg"}).rdd.saveToCassandra(DATABASE, table_name)
 
 
 # df.groupBy('sw_swap', 'experiment_id').count('sw_swap').show()
 # df.groupBy('sw_swap', 'experiment_id').agg({"setup_time":"avg"}).rdd
 # df.select('hw_cpu_mhz').agg(countDistinct('hw_cpu_mhz')).show()
-df.select('hw_cpu_mhz').distinct().count()
+# df.select('hw_cpu_mhz').distinct().count()
