b3a7c3ffbd9ed8c264666e33631f59a586f7ee51
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-01-31 21:28:47 -0800

    load csv files from hdfs and map dataframes to dictionaries to save using pyspark_cassandra

--- spark_batch/experiment_statistics.py
+++ spark_batch/experiment_statistics.py
@@ -8,42 +8,29 @@ conf = SparkConf().setAppName("ExperimentStats").setMaster("spark://ip-172-31-3-
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-RESULTS_DIR='/home/yuguang/Downloads/' #TODO change to HDFS folder
-RESULTS_DIR='/var/datamill/results/'
+RESULTS_DIR='hdfs://ec2-52-89-35-171.us-west-2.compute.amazonaws.com:9000/datamill/'
 DATABASE = 'datamill'
 
 
-os.chdir(RESULTS_DIR)
-for tar_file in os.listdir(RESULTS_DIR):
-    if tar_file.endswith('.tar'):
-        tar = tarfile.open(tar_file)
-        tar.extractall()
-        tar.close()
-        file_name = os.path.basename(tar_file)
-        file_name_parts = file_name.split('_')
-        folder_name = file_name_parts[0] + '_' + file_name_parts[1]
-        experiment_id = file_name_parts[1]
-        header = None
-        with open('{}experiment_{}/{}_results_index.csv'.format(RESULTS_DIR, experiment_id, experiment_id)) as result_csv:
-            reader = csv.reader(result_csv, delimiter=',')
-            for row in reader:
-                if not header:
-                    header = row
-                    break
-        df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('{}experiment_{}/{}_results_index.csv'.format(RESULTS_DIR, experiment_id, experiment_id))
-        for field in [i for i in header if not i.endswith('time')]:
-            if field.startswith('hw') or field.startswith('sw') and df.select(field).distinct().count() > 1:
-                table_name = 'avg_by_' + field
-                df = df.groupBy('experiment_id', field).agg({'setup_time': "avg",'collect_time': "avg",'run_time': "avg"})
-                for time_field in ['setup', 'run', 'collect']:
-                    performance_metric_field = time_field + '_time'
-                    df = df.withColumnRenamed('avg({})'.format(performance_metric_field), performance_metric_field)
-                # df.show()
-                # to avoid bug when saving rdd directly using saveToCassandra
-                def flatten(x):
-                  x_dict = x.asDict()
-                  return x_dict
-                sc.parallelize(df.map(flatten)).saveToCassandra(DATABASE, table_name)
+# os.chdir(RESULTS_DIR)
+# file_list = os.listdir(RESULTS_DIR)
+file_list = ['1525_results_index.csv']
+for file_name in file_list:
+    header = 'experiment_id,job_id,results_file,package_id,package_name,worker_id,config_id,replicate_no,setup_time,run_time,collect_time,hw_cpu_arch,hw_cpu_mhz,hw_gpu_mhz,hw_num_cpus,hw_page_sz,hw_ram_mhz,hw_ram_sz,sw_address_randomization,sw_autogroup,sw_compiler,sw_drop_caches,sw_env_padding,sw_filesystem,sw_freq_scaling,sw_link_order,sw_opt_flag,sw_swap,sw_sys_time'.split(',')
+    df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('{}{}'.format(RESULTS_DIR, file_name))
+    for field in [i for i in header if not i.endswith('time')]:
+        if field.startswith('hw') or field.startswith('sw') and df.select(field).distinct().count() > 1:
+            table_name = 'avg_' + field
+            avg_df = df.groupBy('experiment_id', field).agg({'setup_time': "avg",'collect_time': "avg",'run_time': "avg"})
+            for time_field in ['setup', 'run', 'collect']:
+                performance_metric_field = time_field + '_time'
+                avg_df = avg_df.withColumnRenamed('avg({})'.format(performance_metric_field), performance_metric_field)
+            # avg_df.show()
+            # to avoid bug when saving rdd directly using saveToCassandra
+            def flatten(x):
+              x_dict = x.asDict()
+              return x_dict
+            avg_df.map(flatten).saveToCassandra(DATABASE, table_name)
 
 # df.groupBy('sw_swap', 'experiment_id').count('sw_swap').show()
 # df.groupBy('sw_swap', 'experiment_id').agg({"setup_time":"avg"}).rdd
