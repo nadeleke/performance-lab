c0e8a730900925f3e5c82c1d8043d59e75f5197c
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-02-01 19:59:30 -0800

    display chart and dropdown

--- webserver/webserver/views.py
+++ webserver/webserver/views.py
@@ -11,9 +11,12 @@ def attrs(model):
 def experiment(request, id):
     avg_dict = {}
     id = 1525
+    first_factor = None
     for model in [avg_hw_cpu_arch, avg_hw_cpu_mhz, avg_hw_gpu_mhz, avg_hw_num_cpus, avg_hw_page_sz, avg_hw_ram_mhz, avg_hw_ram_sz, avg_sw_address_randomization, avg_sw_autogroup, avg_sw_compiler, avg_sw_drop_caches, avg_sw_env_padding, avg_sw_filesystem, avg_sw_freq_scaling, avg_sw_link_order, avg_sw_opt_flag, avg_sw_swap, avg_sw_sys_time]:
         items = model.objects.filter(experiment_id=id)
         for i in items:
+            if not first_factor:
+                first_factor = model.__name__
             field = model.__name__.replace('avg_', '')
             avg_dict.setdefault(field, [])
             avg_dict[field].append({
@@ -23,4 +26,4 @@ def experiment(request, id):
                 'run': i.run_time
             })
 
-    return render(request, 'experiment.html', context={'json': json.dumps(avg_dict)})
+    return render(request, 'experiment.html', context={'json': json.dumps(avg_dict), 'factors': avg_dict.keys(), 'first_factor': first_factor})
