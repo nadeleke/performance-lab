eb6197a7514000d827365f9970e9669787d36d25
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-02-04 22:21:18 -0800

    change folder before uploading to s3

--- ingestion/s3/upload.py
+++ ingestion/s3/upload.py
@@ -14,18 +14,19 @@ os.chdir(RESULTS_DIR)
 for tar_file in os.listdir(RESULTS_DIR):
     if tar_file.endswith('_results.tar') and tar_file.startswith(
 'experiment_'):
+        print tar_file
+        tar = tarfile.open(tar_file)
+        tar.extractall()
+        tar.close()
+        file_name = os.path.basename(tar_file)
+        file_name_parts = file_name.split('_')
+        folder_name = file_name_parts[0] + '_' + file_name_parts[1]
+        experiment_id = file_name_parts[1]
+        os.chdir('{}experiment_{}/'.format(RESULTS_DIR, experiment_id, experiment_id))
+        csv_path = '{}_results_index.csv'.format(experiment_id)
+        s3_client.upload_file(csv_path, 'yuguang-experiments', csv_path)
+        os.chdir(RESULTS_DIR)
         try:
-            print tar_file
-            tar = tarfile.open(tar_file)
-            tar.extractall()
-            tar.close()
-            file_name = os.path.basename(tar_file)
-            file_name_parts = file_name.split('_')
-            folder_name = file_name_parts[0] + '_' + file_name_parts[1]
-            experiment_id = file_name_parts[1]
-            csv_path = '{}_results_index.csv'.format(experiment_id)
-            s3_client.upload_file(csv_path, 'yuguang-data', csv_path)
-            os.rmdir(tar_file.replace('.tar', ''))
+            os.rmdir(tar_file.replace('_results.tar', ''))
         except:
-            # older experiment files have a different structure
             pass
