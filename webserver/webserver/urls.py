f462c663c769c6111fc0892684609a31aeada107
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-02-10 21:18:41 -0800

    add link to view jobs

--- webserver/webserver/urls.py
+++ webserver/webserver/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.home, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^experiment/([0-9]{2,4})/$', views.experiment),
-    url(r'^experiments/$', views.experiment_list, name='experiment_list')
+    url(r'^experiments/$', views.experiment_list, name='experiment_list'),
 ]
