8c8662312a2a364baf39898d69a1f7b9b50c1315
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-02-18 21:46:01 -0800

    update kafka topic

--- spark_stream/src/main/scala/com/hastyexperiment/ExperimentResultStream.scala
+++ spark_stream/src/main/scala/com/hastyexperiment/ExperimentResultStream.scala
@@ -105,7 +105,7 @@ object ExperimentResultStream {
     Logger.getLogger("akka").setLevel(Level.OFF)
 
     val brokers = "ec2-52-36-57-191.us-west-2.compute.amazonaws.com:9092"
-    val topics = "m40"
+    val topics = "m42"
     val topicsSet = topics.split(",").toSet
 
     // Create context with 2 second batch interval
